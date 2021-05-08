fda4e2e0bb05ed1b2c90e6a1f5cf9e93eb46a3b3
Brenna McLeod <brenna.mcleod94@gmail.com>
2021-05-08 11:06:53 -0500

    added files and dependencies needed for tailwindcss, including CRACO which overrides default react styling. Sample login code added to App.js as placeholder

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // Allows tailwind to tree-shake unused styles in production builds:
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
