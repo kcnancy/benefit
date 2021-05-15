5cee4972bb14bde92ac81aaf30e6ad2d85f57fcb
dannauu <scatterman16@gmail.com>
2021-05-15 10:59:59 -0500

    added passport and features, added model and routes, and connection to db

---
+++ models/user.js
@@ -0,0 +1,15 @@
+// dependencies
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+// Create Model
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+    _id: { type: String, auto: true},
+    email: {type: String, required: true, unique: true},
+});
+// Export Model
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('userData', User, 'userData');
